b8e7c16092fb8f58c32fa8df52359319416f4619
Rapido01 <Chidozie.osuji@yahoo.com>
2024-05-23 19:24:05 +0500

    Identity Framework Implementation - Functionality Data loss

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementProject.Models;
+
+public class Category
+{
+	public int ID { get; set; }
+	[Required(ErrorMessage = "Name is required!")]
+	public string? Name { get; set; }
+	[Required(ErrorMessage = "Description is required!")]
+	public string? Description { get; set; }
+	[Required(ErrorMessage = "No of tasks required!"),
+	Range(0, int.MaxValue, ErrorMessage = "Atleast initialize a task!")]
+	public int NoOfTasks { get; set; }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TaskManagementProject.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DBTaskManagementContextSampleConnection") ?? throw new InvalidOperationException("Connection string 'DBTaskManagementContextSampleConnection' not found.");
+
+builder.Services.AddDbContext<DBTaskManagementContextSample>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<TaskManagementProjectUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<DBTaskManagementContextSample>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -19,5 +27,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
