1836ec9129a3d73105690a77b975d15aa7f4c6e8
Rapido01 <Chidozie.osuji@yahoo.com>
2024-05-25 13:01:19 +0500

    Category View

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagementProject.Models;
+
+namespace TaskManagementProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly TaskManagementDbContext _context;
+
+        public TasksController(TaskManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Tasks
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tasks>>> GetTasks()
+        {
+            return await _context.Tasks.ToListAsync();
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tasks>> GetTasks(int id)
+        {
+            var tasks = await _context.Tasks.FindAsync(id);
+
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+
+            return tasks;
+        }
+
+        // PUT: api/Tasks/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTasks(int id, Tasks tasks)
+        {
+            if (id != tasks.TaskId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tasks).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TasksExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Tasks
+        [HttpPost]
+        public async Task<ActionResult<Tasks>> PostTasks(Tasks tasks)
+        {
+            _context.Tasks.Add(tasks);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTasks", new { id = tasks.TaskId }, tasks);
+        }
+
+        // DELETE: api/Tasks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTasks(int id)
+        {
+            var tasks = await _context.Tasks.FindAsync(id);
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(tasks);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TasksExists(int id)
+        {
+            return _context.Tasks.Any(e => e.TaskId == id);
+        }
+    }
+}
